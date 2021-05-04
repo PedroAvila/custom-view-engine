04384c8f32407a26745d55e3cab95c8bdbeac4be
PedroAvila <minimainor_austin@hotmail.com>
2021-05-04 07:53:59 -0500

    Creación de motores de vista personalizados - parte 3

--- ViewEngine/CustomView.cs
+++ ViewEngine/CustomView.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 
@@ -18,6 +19,41 @@ namespace CustomViewEngine.ViewEngine
             var ViewFileContent = File.ReadAllText(ViewFilePath);
             string htmlContent = GetHTML(ViewFileContent);
             writer.Write(htmlContent);
+        }
+
+        private string GetHTML(string viewFileContent)
+        {
+            string result = viewFileContent;
+
+            string patternControl =
+                "<" +
+                "(?<Control>[a-z]+)" +
+                ".*" +
+                "?/>";
+            result = Regex.Replace(viewFileContent, patternControl, ControlCodeGenerator, RegexOptions.IgnoreCase);
+
+            return result;
+        }
+
+        private string ControlCodeGenerator(Match currentMatch)
+        {
+            string value = currentMatch.Value;
+            string control = currentMatch.Result("${Control}");
+            switch (control.ToLower())
+            {
+                case "datagrid":
+                    if (Model != null)
+                    {
+                        value = RenderDataGrid(value);
+                    }
+                    break;
+                case "time":
+                    value = DateTime.Now.ToShortTimeString();
+                    break;
+                default:
+                    break;
+            }
+            return value;
         }
     }
 }
