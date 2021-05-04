925a8279803321972c599cf738b8d35f43416676
PedroAvila <minimainor_austin@hotmail.com>
2021-05-04 08:45:19 -0500

    Creación de motores de vista personalizados - parte 4

--- ViewEngine/CustomView.cs
+++ ViewEngine/CustomView.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
@@ -52,6 +54,59 @@ namespace CustomViewEngine.ViewEngine
                     break;
                 default:
                     break;
+            }
+            return value;
+        }
+
+        private string RenderDataGrid(string value)
+        {
+            string pattern =
+                "{Binding\\s+" +
+                "(?<Items>[a-z_][a-z0-9_]*)" +
+                "\\s*}";
+            var r = new Regex(pattern, RegexOptions.IgnoreCase);
+            var m = r.Match(value);
+            if (m.Success)
+            {
+                string bindingExpression = value;
+                string itemsPropertyName = m.Result("${Items}");
+                var sb = new StringBuilder();
+                try
+                {
+                    var items = Model.GetType().GetProperty(itemsPropertyName).GetValue(Model) as IEnumerable;
+                    if (items != null)
+                    {
+                        sb.Append("<table class=\"DataGrid\">");
+                        string[] propertyNames = null;
+                        bool alt = false;
+                        foreach (var item in items)
+                        {
+                            if (propertyNames == null)
+                            {
+                                propertyNames = item.GetType().GetProperties().Select(p => p.Name).ToArray();
+                                sb.Append("<tr>");
+                                foreach (var e in propertyNames)
+                                {
+                                    sb.AppendFormat("<th>{0}</th>", e);
+                                }
+                                sb.Append("</tr>");
+                            }
+                            sb.AppendFormat("<tr {0}>", alt ? "class=\"Alt\"" : "");
+                            foreach (var e in propertyNames)
+                            {
+                                sb.AppendFormat("<td>{0}</td>", item.GetType().GetProperty(e).GetValue(item));
+                            }
+                            sb.Append("</tr>");
+                            alt = !alt;
+                        }
+                        sb.Append("</table>");
+                        value = sb.ToString();
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
             }
             return value;
         }
