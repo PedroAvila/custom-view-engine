d8bcfde5f0b6760c4f4f53d6ae7aad6df272bcd6
PedroAvila <minimainor_austin@hotmail.com>
2021-05-04 07:00:55 -0500

    Creación de motores de vista personalizados - parte 1

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using CustomViewEngine.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomViewEngine.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            var vm = new Store();
+            return View(vm);
+        }
+    }
+}
---
+++ ViewModels/Store.cs
@@ -0,0 +1,34 @@
+﻿using CustomViewEngine.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomViewEngine.ViewModels
+{
+    public class Store
+    {
+        public Store()
+        {
+            Products = new List<Product>
+            {
+                new Product {ID = 1, ProductName = "Azúcar", UnitPrice = 10, UnitsInStock = 100},
+                new Product {ID = 2, ProductName = "Leche", UnitPrice = 13, UnitsInStock = 100},
+                new Product {ID = 3, ProductName = "Huevos", UnitPrice = 167, UnitsInStock = 100},
+                new Product {ID = 4, ProductName = "Arroz", UnitPrice = 23, UnitsInStock = 1500},
+                new Product {ID = 5, ProductName = "Frijol", UnitPrice = 10, UnitsInStock = 1100}
+            };
+        }
+
+        public List<Product> Products { get; set; }
+
+        public IEnumerable IDs
+        {
+            get
+            {
+                return Products.Select(p => new { p.ID, p.ProductName }).ToArray();
+            }
+        }
+    }
+}
